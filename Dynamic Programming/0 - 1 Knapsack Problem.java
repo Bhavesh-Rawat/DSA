dd7cfa6b8849bd182908d2e8f542591cc1cba795
Bhavesh Rawat <78506730+Bhavesh-Rawat@users.noreply.github.com>
2022-10-30 01:04:30 +0530

    2d D.P 1st prblm

--- Dynamic Programming/0 - 1 Knapsack Problem.java
+++ Dynamic Programming/0 - 1 Knapsack Problem.java
@@ -1,7 +1,7 @@
-
+// gfg link for prblm 👇
 // https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
 
-
+// '😤' Brute Force 👇 Obviusly TA
 // class Solution 
 // { 
 //     //Function to return max value that can be put in knapsack of capacity W.
@@ -18,7 +18,7 @@
 //         int currentItemProfit = profit[currentItem];
 //         int consider = 0;
         
-//         if(currentItemWeight <= capacity)
+//         if(currentItemWeight <= capacity)                👇required things to get profit  
 //         consider = currentItemProfit + maxProfit(weight,profit,currentItem+1,capacity,n);
         
 //         int notConsider = maxProfit(weight,profit,currentItem+1,capacity,n);
@@ -26,3 +26,38 @@
 //         return Math.max(consider,notConsider);
 //     }
 // }
+
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int capacity, int weights[], int values[], int n) 
+    { 
+         return maxProfit(weights,values,capacity,n,0,new HashMap<String,Integer>());
+    }                                                                                                  
+                                                                                                    //    👇<currentKey, Value>
+   
+    public static int maxProfit(int[] weights,int[] values,int capacity,int n,int currentItem,HashMap<String,Integer> memo)
+    {
+        if(currentItem >= n)
+            return 0;
+
+                //         As 2 key are there if they are integer they may conflict and mispatch 
+                //   👇      thus making it String         // 👇Delimiter/separator
+                                                            
+        String currentKey = Integer.toString(currentItem) + "_" + Integer.toString(capacity);
+        
+        if(memo.containsKey(currentKey))
+            return memo.get(currentKey);
+        
+        int consider = 0;
+        if(weights[currentItem] <= capacity){
+            consider = values[currentItem] + maxProfit(weights, values, capacity - weights[currentItem], n, currentItem + 1,memo);
+        } 
+        
+        int notConsider = maxProfit(weights, values, capacity, n, currentItem + 1,memo);
+        
+        memo.put(currentKey,Math.max(consider,notConsider));
+        
+        return memo.get(currentKey);
+    }
+}

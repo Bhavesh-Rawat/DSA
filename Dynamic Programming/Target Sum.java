47424299bf662bb1fdad7d2473dc0fbf8e63697f
Bhavesh Rawat <78506730+Bhavesh-Rawat@users.noreply.github.com>
2022-11-17 00:25:40 +0530

    Target Sum Code

--- Dynamic Programming/Target Sum.java
+++ Dynamic Programming/Target Sum.java
@@ -1,19 +1,45 @@
+// Problem link - https://leetcode.com/problems/target-sum/description/
+
 // Solution using brute force -
 
+// class Solution {
+//     public int findTargetSumWays(int[] nums, int target) {
+//         return totalways (nums,0,target);
+        
+//     }
+//     private int totalways(int[]nums,int currentindex,int target) {
+//         if(currentindex == nums.length && target == 0)
+//             return 1;
+//         if(currentindex == nums.length && target != 0)
+//             return 0;
+        
+//         int ifsum = totalways (nums,currentindex +1, target - nums[currentindex]);
+//         int ifnotsum = totalways (nums,currentindex +1, target + nums[currentindex]);
+//         return ifsum + ifnotsum;
+            
+//         }
+// }
+
+// Optimized Code -
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return totalways (nums,0,target);
+        return totalways (nums,0,target, new HashMap<String,Integer>());
         
     }
-    private int totalways(int[]nums,int currentindex,int target) {
+    private int totalways(int[]nums,int currentindex,int target, HashMap<String,Integer> memo) {
         if(currentindex == nums.length && target == 0)
             return 1;
         if(currentindex == nums.length && target != 0)
             return 0;
         
-        int ifsum = totalways (nums,currentindex +1, target - nums[currentindex]);
-        int ifnotsum = totalways (nums,currentindex +1, target + nums[currentindex]);
-        return ifsum + ifnotsum;
-            
+        String currentKey = Integer.toString(currentindex)+"-"+ Integer.toString(target);
+
+        if (memo.containsKey(currentKey))
+        return memo.get(currentKey);
+
+        int ifsum = totalways (nums,currentindex +1, target - nums[currentindex],memo);
+        int ifnotsum = totalways (nums,currentindex +1, target + nums[currentindex],memo);
+        memo.put(currentKey,ifsum + ifnotsum);
+            return memo.get(currentKey);
         }
 }
